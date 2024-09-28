c8929ff0e8f7951502facb18b9ab0711080d56f5
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-09-28 15:32:56 +0300

    fill the themeProfider and rename the files, correct spelling

---
+++ src/components/ThemeProvider/ThemeProvider.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import { ThemeContext } from '../ThemeContext/ThemeContext'
+
+const ThemeProvider = ({ children }) => {
+    const [theme, setTheme] = useState('light')
+    const toggleTheme = () => {
+        setTheme((prevTheme) => prevTheme === 'light' ? 'dark' : 'light')
+    }
+    return(
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+export default ThemeProvider
